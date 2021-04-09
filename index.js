5505076703ec5042e13e9531014a1f577329e793
Cassandra <cparisi1290@gmail.com>
2021-04-09 15:48:12 -0400

    create event listener for form submit button, create function for handling submit inputs

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const endPoint = "http://localhost:3000/api/v1/plants"
 document.addEventListener('DOMContentLoaded', () => {
-    fetchPlants();  
+    fetchPlants()
+
+    const plantForm = document.querySelector("#create-plant-form")
+
+    plantForm.addEventListener("submit", (e) => formHandler(e))
 })
 
 function fetchPlants() {
     fetch(endPoint)
     .then(resp => resp.json())
     .then(plants => {
-        console.log(plants);
         plants.data.forEach(plant => {
             const plantMarkup = `
                 <div data-id=${plant.id}>
@@ -26,4 +29,23 @@ function fetchPlants() {
             document.querySelector('#plant-container').innerHTML += plantMarkup
         })
     })
+}
+
+function formHandler(e) {
+    e.preventDefault()
+    console.log(e)
+    debugger
+    const nameInput = document.querySelector("#name-input").value
+    const wateringDayInput = document.querySelector("#watering-day-input").value
+    const roomInput = parseInt(document.querySelector("#rooms").value)
+    const lightInput = document.querySelector("#light-input").value
+    const waterInput = document.querySelector("#water-input").value
+    const foodInput = document.querySelector("#food-input").value
+    const humidityInput = document.querySelector("#humidity-input").value
+    const temperatureInput = document.querySelector("#temperature-input").value
+    const toxicityInput = document.querySelector("#toxicity-input").value
+    const additionalCareInput = document.querySelector("#additional-care-input").value
+    const urlInput = document.querySelector("#url-input").value
+
+    postFetch()
 }
