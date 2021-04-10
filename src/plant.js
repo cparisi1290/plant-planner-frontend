502df5b66ef7f77fcddc47473a110d66859eb023
Cassandra <cparisi1290@gmail.com>
2021-04-10 09:32:03 -0400

    move buttons outside of div

--- src/plant.js
+++ src/plant.js
@@ -29,9 +29,10 @@ class Plant {
                 <p><b>Temperature:</b> ${this.temperature}</p>
                 <p><b>Toxicity:</b> ${this.toxicity}</p>
                 <p><b>Additional Care:</b> ${this.additional_care}</p>
+                </div> 
                 <button class="edit" data-id=${this.id}>Edit</button>
                 <button class="delete" data-id=${this.id}>Delete</button>
-            </div> <br><br>
+                <br><br>
         `;
     }
 }
