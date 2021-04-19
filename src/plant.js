d1ee60901e56435eef57a5717beb0e553a16d95e
Cassandra <cparisi1290@gmail.com>
2021-04-19 17:07:36 -0400

    create delete button, append it to plant card

--- src/plant.js
+++ src/plant.js
@@ -16,23 +16,42 @@ class Plant {
         Plant.all.push(this)
     } 
 
+    deletePlant(id) {
+        const deleteButton = document.createElement('button')
+        deleteButton.className = "delete"
+        deleteButton.id = id
+        deleteButton.innerText = "delete"
+        // `<button type="button" class="delete" data-id=${id}>Delete</button><br><br><br><br></br>`
+        deleteButton.addEventListener('click', (e) => deleteHandler(e))
+        console.log(deleteButton)
+        return deleteButton
+    }
+
     renderPlantCard() {
-        return `
-            <div data-id=${this.id}>
+        
+        let plant = document.createElement('div')
+        plant.className = plant.id
+
+        let body = `
                 <img src=${this.img_url} height="200" width="250">
                 <h1>${this.name}</h1>
                 <h2>Watering Day: ${this.watering_day} - Location: ${this.room.name}</h2>
                 <h3>Plant Info</h3>
                 <p><b>Light:</b> ${this.light}</p>
                 <p><b>Water:</b> ${this.water}</p>
+                <p><b>Food:</b> ${this.food}</p>
                 <p><b>Humidity:</b> ${this.humidity}</p>
                 <p><b>Temperature:</b> ${this.temperature}</p>
                 <p><b>Toxicity:</b> ${this.toxicity}</p>
                 <p><b>Additional Care:</b> ${this.additional_care}</p>
-                </div> <br>
-                <button type="button" class="delete" data-id=${this.id}>Delete</button><br><br><br><br>
             `;
+        plant.innerHTML = body
+        plant.append(this.deletePlant(this.id))
+        console.log(plant)
+        return plant
     }
+
+
 
     // static findById(id) {
     //     return this.all.find(plant => plant.id === id);
